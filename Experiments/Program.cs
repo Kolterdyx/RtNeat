f0f8631ddb0759cff8d6b922c16e39d088018a92
Ciro García Belmonte <ciro.garcia.belmonte@gmail.com>
2023-03-07 01:01:53 +0100

    missing project

---
+++ Experiments/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using RtNeat;
+
+var config = new Configuration()
+{
+    InputCount = 2,
+    OutputCount = 1
+};
+var net = new Network(config);
+
+for (int i = 0; i < 100; i++)
+{
+    net.Mutate();
+}
+
+Console.WriteLine(JsonSerializer.Serialize(net));
